bc0b0f8355152627a0d58b15db0fa14524f98ee1
logan42474 <loganmiller040402@gmail.com>
2020-02-07 16:23:56 -0700

    Added "Map" imbedded classes
    More skeletons!!!

--- FRC2020/src/main/java/frc/robot/Map.java
+++ FRC2020/src/main/java/frc/robot/Map.java
@@ -5,5 +5,20 @@ package frc.robot;
  */
 public class Map {
     // Where all controllers and gamepads are stored
+    public static class driveTrain{
+        //Drive Train Motors
+
+    }
+    public static class Controllers{
+        //Controllers
+
+    }
+    public static class Turret{
+        //Turret Motors
     
+    }
+    public static class Cartridge{
+        // Cartridge / Loading-Arm motors (and pneumatics)
+
+    }
 }
