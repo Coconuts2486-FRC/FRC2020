929a87f0de62efd405993265ee7314e0fff601c2
Owen Krueger <okthescientist@gmail.com>
2020-02-08 12:23:32 -0700

    Start of piston movement

--- FRC2020/src/main/java/frc/robot/Cartridge/pistonlift.java
+++ FRC2020/src/main/java/frc/robot/Cartridge/pistonlift.java
@@ -1,10 +1,21 @@
-/*public class pistonlift {
-    public static void piston(){
-    if (joystick.getRawButton) {
-        RightPiston.set(true);
-    }else if{
-        RightPiston.set(false);   
+package frc.robot.Cartridge;
+
+import frc.robot.Map;
+
+public class pistonlift {
+    public static void pistons() {
+        if (Map.Controllers.driverLeft.getRawButton(1) || Map.Controllers.driverRight.getRawButton(1)) {
+            Map.Cartridge.RightPiston.set(true);
+            Map.Cartridge.LeftPiston.set(true);
+        } else {
+            Map.Cartridge.RightPiston.set(false);
+            Map.Cartridge.LeftPiston.set(false);
+        }
+       
+    }
+    public static void PistonRoller() {
+       if (Map.Controllers.driverLeft.getRawButton(1) || Map.Controllers.driverRight.getRawButton(1) ) {
+            //Map.Cartridge.ArmRoller.set(ContolMode.PercentOutput,1);
+       }
     }
 }
-}
-*/
