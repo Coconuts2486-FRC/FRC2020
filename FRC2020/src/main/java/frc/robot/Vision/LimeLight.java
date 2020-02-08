1380bcc615d866dc1e25fb9f4a492cd1daeb780a
logan42474 <loganmiller040402@gmail.com>
2020-02-08 14:41:25 -0700

    Updated LimeLight

--- FRC2020/src/main/java/frc/robot/Vision/LimeLight.java
+++ FRC2020/src/main/java/frc/robot/Vision/LimeLight.java
@@ -1,21 +1,34 @@
 package frc.robot.Vision;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 /**
  * LimeLight
  */
 public class LimeLight {
+    private static NetworkTable table = NetworkTableInstance
+    .getDefault().getTable("limelight"); // might have to be updated in methods
 
     public static double getX(){
         // Returns the X axis of the target
-        return 0;
+        String tx = table.getEntry("tx0").toString();
+        return Double.parseDouble(tx);
     }
     public static double getY(){
         // Returns the Y axis of the target
-        return 0;
+        String ty = table.getEntry("ty0").toString();
+        return Double.parseDouble(ty);
     }
     public static boolean isTarget(){
         // Returns true if there is a target in frame
-        return false;
+        String tv = table.getEntry("tv").toString();
+        if(tv.equals("1")){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     static class LED{
