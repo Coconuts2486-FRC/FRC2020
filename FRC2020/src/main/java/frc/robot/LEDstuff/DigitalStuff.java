93ecf7eba030276103af9d39398547b1624312f4
Owen Krueger <okthescientist@gmail.com>
2020-02-19 16:29:02 -0700

    added some sensor code AGAIN

---
+++ FRC2020/src/main/java/frc/robot/LEDstuff/DigitalStuff.java
@@ -0,0 +1,23 @@
+package frc.robot.LEDstuff;
+import edu.wpi.first.wpilibj.PWM;
+public class DigitalStuff{
+private static PWM Red = new PWM(0);
+private static PWM Blue = new PWM(1);
+private static PWM Green = new PWM(2);
+private static void test(){
+    int R;
+    int B;
+    int G ;
+    Red.setBounds(100, 100, 50, 0, 0);
+    Red.setRaw(50);
+    Blue.setBounds(100, 100, 50, 0, 0);
+    Blue.setRaw(50);
+    Green.setBounds(100, 100, 50, 0, 0);
+    Green.setRaw(50);
+
+    
+}
+    
+
+
+}
